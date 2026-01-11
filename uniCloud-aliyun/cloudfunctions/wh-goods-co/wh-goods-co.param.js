cbef5050a1033a24ab3c0bf9fad93d6b9d8a0f73
songxu <>
2026-01-11 22:02:35 +0800

    test: simplify param file format

--- uniCloud-aliyun/cloudfunctions/wh-goods-co/wh-goods-co.param.js
+++ uniCloud-aliyun/cloudfunctions/wh-goods-co/wh-goods-co.param.js
@@ -1,11 +1,5 @@
-// wh-goods-co 云对象测试参数
-// 文档: https://uniapp.dcloud.net.cn/uniCloud/cloud-obj.html#test
-
 module.exports = {
-  // 测试方法名（必填）
   methodName: 'getGoodsList',
-
-  // 测试参数（选填）
   methodParams: {
     page: 1,
     limit: 10
